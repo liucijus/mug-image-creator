767e22d0d4a91876ee77a0dde3f0ddb17aad5fb8
vaidas <vaidas.pilkauskas@gmail.com>
2013-12-10 19:54:07 +0200

    refactoring: moved logo image to private space created configuration location for the image removed unused bootstrap theme css fixed javascript routing

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,20 +4,17 @@ import play.api._
 import play.api.mvc._
 import lt.overdrive.mugs.imagegenerator.ImageGenerator
 import javax.imageio.ImageIO
-import java.io.{ByteArrayOutputStream, OutputStream, BufferedOutputStream}
-import scalax.io.managed.WritableByteChannelResource
-import play.api.http.Writeable
-import play.core.Router
+import java.io.ByteArrayOutputStream
 
 object Application extends Controller {
 
-  def index(text: String) = Action {
+  def index(text: String) = Action { implicit req: RequestHeader =>
     Ok(views.html.index(text))
   }
 
   def render(text: String, department: String) = Action {
     import play.api.Play.current
-    val file = Play.application.getFile("public/images/sl_c_pos_letter_250px_A4.png")
+    val file = Play.application.getFile(Play.configuration.getString("logo.image").get)
     val image = ImageGenerator.generate(ImageIO.read(file), text, department)
     val out = new ByteArrayOutputStream()
     ImageIO.write(image, "PNG", out)
